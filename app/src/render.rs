a1a40e177144b6c01ab6e1bab4b171442b34fb62
HashDankHog <madelinecooreman@gmail.com>
2026-07-20 23:50:50 -0600

    add frame struct and methods

--- app/src/render.rs
+++ app/src/render.rs
@@ -1,50 +1,74 @@
+use std::ops::ControlFlow::{Break, Continue};
+
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize)]
-pub struct Color(pub u8,pub u8,pub u8);
+pub struct Color(pub u8,pub u8,pub u8, pub u8);
 
 #[derive(Serialize, Deserialize, Clone)]
-pub struct Image {
-    pub canvas: Vec<Vec<Color>>,
+pub struct Frame {
+    pub pixels: Vec<u8>,
     width: usize,
     height: usize,
 }
-impl Default for Image {
+impl Default for Frame {
     fn default() -> Self {
-        Image { 
-            canvas: vec![vec![Color(0,0,0)]],
+        Frame { 
+            pixels: vec![0,0,0,255],
             width:  1,
             height: 1 
         }
     }
 }
-impl From<(usize, usize)> for Image {
-    fn from(size: (usize,usize)) -> Self {
-        Image {
-            canvas: vec![vec![Color(0,0,0); size.0]; size.1],
-            width: size.0,
-            height: size.1,
+impl From<(usize, usize)> for Frame {
+    fn from(value: (usize, usize)) -> Self {
+        let mut vec = Vec::new();
+        for _ in 0..value.0*value.1 {
+            vec.push(0);
+            vec.push(0);
+            vec.push(0);
+            vec.push(255);
         }
+        Frame { pixels: vec, width: value.0, height: value.1 }
     }
 }
-impl Image {
-    pub fn draw_pixel(&mut self, position: (usize, usize), pixel: Color) {
-        if position.0 > self.width || position.1 > self.height {
-            panic!("cannot draw to pixel outside of sceen size");
+impl Frame {
+    pub fn pixel(&self, pixel: (usize, usize)) -> Result<Color, &'static str> {
+        if pixel.0 > self.width || pixel.1 > self.height {
+            Err("attempted to read pixel outside of bounds")
+        } else {
+            Ok(Color(
+                self.pixels[4*(self.width*pixel.0+pixel.1)+0],
+                self.pixels[4*(self.width*pixel.0+pixel.1)+1],
+                self.pixels[4*(self.width*pixel.0+pixel.1)+2], 
+                self.pixels[4*(self.width*pixel.0+pixel.1)+3],
+            ))
         }
-        self.canvas[position.0][position.1]=pixel;
     }
-    pub fn to_canvas(&self) -> Vec<u8> {
-        let mut image_data = Vec::new();
-        for row in 0..self.height {
-            for colum in 0..self.width {
-                image_data.push(self.canvas[row][colum].0);
-                image_data.push(self.canvas[row][colum].1);
-                image_data.push(self.canvas[row][colum].2);
-                image_data.push(255);
+    pub fn set_pixel(&mut self, pixel: (usize, usize), color: Color) {
+        if pixel.0 > self.width || pixel.1 > self.height {
+        } else {
+            self.pixels[4*(self.width*pixel.0+pixel.1)+0] = color.0;
+            self.pixels[4*(self.width*pixel.0+pixel.1)+1] = color.1;
+            self.pixels[4*(self.width*pixel.0+pixel.1)+2] = color.2;
+            self.pixels[4*(self.width*pixel.0+pixel.1)+3] = color.3;
+        }
+    }
+    pub fn clear(&mut self) {
+        self.pixels = vec![0; self.pixels.len()];
+    }
+    pub fn draw_rect(&mut self, coord: (usize, usize), size: (usize, usize), color: Color) {
+        if coord.0 > self.width || coord.1 > self.height || coord.0+size.0> self.width || coord.1+size.1> self.height {
+            return;
+        }
+        for height in coord.1..=(coord.1+size.1) {
+            for width in coord.0..=(coord.0+size.0) {
+                self.set_pixel((width, height), color.clone());
             }
         }
-        image_data
+    }
+    pub fn draw_line(&mut self, _point1: (usize, usize), _point2: (usize, usize)) {
+        unimplemented!()
     }
 }
 
